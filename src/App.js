19255d712ee37ed5f3ab455f5effe99786a662d7
nicoon177 <nicoon177@gmail.com>
2018-02-13 22:50:34 +0200

    Add files via upload
    searched

--- src/App.js
+++ src/App.js
@@ -28,37 +28,42 @@ class App extends Component {
     render() {
         const { twits, name } = this.state;
         return (
-            <div>
+            <div className='twits'>
                 <div className='search-twit'>
-                    <input type="text" placeholder='Enter search' onChange={this.handleNameChange}/>
-                    <Link to={`/Search/${name}`}>
-                        Search
-                    </Link>
+                    <form action="">
+                        <input className='search' type="text" placeholder='Enter search' onChange={this.handleNameChange}/>
+                        <Link to={`/Search/${name}`}>
+                            <input className='btn-search' type="submit" value='Search'/>
+                        </Link>
+                    </form>
                 </div>
-                {
-                    twits.map(function (item) {
-                        return (
-                            <div className='twit' key={item.id}>
-                                <div className="header_twit">
-                                    <div className="header-img">
-                                        <img src={item.user.profile_image_url} alt='title'/>
-                                    </div>
-                                    <div className="header-content">
-                                        <h4 className="header-name">
-                                        {
-                                            <a href="">{item.user.name} <span>{item.user.screen_name}</span></a>
-                                        }
-                                            <span className='header-data'>{item.created_at}</span>
-                                        </h4>
-                                        <div className="twit_content">
-                                            <p>{item.text}</p>
+                <div>
+                    {
+                        twits.map(function (item) {
+                            return (
+                                <div className='twit' key={item.id}>
+                                    <div className="header_twit">
+                                        <div className="header-img">
+                                            <img src={item.user.profile_image_url} alt='title'/>
+                                        </div>
+                                        <div className="header-content">
+                                            <h4 className="header-name">
+                                                {
+                                                    <a href="">{item.user.name} <span>{item.user.screen_name}</span></a>
+                                                }
+                                                <span className='header-data'>{item.created_at}</span>
+                                            </h4>
+                                            <div className="twit_content">
+                                                <p>{item.text}</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
+                </div>
+
             </div>
         )
     }
--- src/Search.js
+++ src/Search.js
@@ -1,34 +1,69 @@
 import React, {Component} from 'react';
 
 
-
 class Search extends Component {
-
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             name: props.match.params.name,
-            twits: {}
+            twit: [],
+
         }
     }
 
     componentDidMount() {
-        fetch(`api/search?q=${this.state.name}`)
+        fetch(`/api/search?q=${this.state.name}`)
             .then(response => response.json())
             // .then(json => console.log(json))
             .then(json => {
                 this.setState({
-                    twits: json.statuses,
+                    twit: json.statuses,
                 })
             })
     }
 
 
+    handleNameChange(event) {
+        this.setState({name: event.target.value});
+    }
 
     render() {
+        const {twit} = this.state;
         return (
             <div>
-                <h2>the page serched. Name = {this.state.name}</h2>
+                <div className='search-twit'>
+                    <form>
+                        <input className='search' type="text" placeholder='Enter search' onChange={this.handleNameChange} />
+                        <input className='btn-search' type="submit" value='Search' />
+                    </form>
+                </div>
+                <div>
+                    {
+                        twit.map(function (item) {
+                            return (
+                                <div className='twit' key={item.id}>
+                                    <div className="header_twit">
+                                        <div className="header-img">
+                                            <img src={item.user.profile_image_url} alt='title'/>
+                                        </div>
+                                        <div className="header-content">
+                                            <h4 className="header-name">
+                                                {
+                                                    <a href="">{item.user.name} <span>{item.user.screen_name}</span></a>
+                                                }
+                                                <span className='header-data'>{item.created_at}</span>
+                                            </h4>
+                                            <div className="twit_content">
+                                                <p>{item.text}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
             </div>
         )
     }
